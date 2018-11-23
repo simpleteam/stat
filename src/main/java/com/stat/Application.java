f07ab6aa1e5144f6c34f6009da1d2ccd2b4647a0
sashabogdan <sashabogdan@ukr.net>
2018-11-23 19:37:47 +0200

    set up embedded database

--- src/main/java/com/stat/Application.java
+++ src/main/java/com/stat/Application.java
@@ -3,10 +3,12 @@ package com.stat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class},
+	scanBasePackages = "com")
 @ComponentScan("com.")
 @EnableJpaRepositories("com.dao")
 @EntityScan("com.domain")
